bee7d3f2e01c0c7dc43f24e16a632ee481211685
gamemann <gamemann@gflclan.com>
2020-12-27 23:57:39 +0000

    Begin wrok on xdpfwd-add.

--- src/xdpfwd-add.c
+++ src/xdpfwd-add.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <bpf.h>
+
+#include "xdp_prog.h"
+
+int bpf_map_get(const char *path)
+{
+    int fd = -1;
+
+    fd = bpf_obj_get(path);
+
+    return fd;
+}
 
 int main(int argc, char *argv[])
 {
+    int fwdmap = bpf_map_get(FORWARD_MAP);
 
+    if (fwdmap < 0)
+    {
+        fprintf(stderr, "Coult not retrieve forward map FD. Exiting...\n");
+        
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
 }
