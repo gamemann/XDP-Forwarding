97458eee32bce178308eaba1719f682567d10cf4
gamemann <gamemann@gflclan.com>
2020-12-28 00:30:02 +0000

    Add config source and header files.

---
+++ src/config.c
@@ -0,0 +1,7 @@
+#include <libconfig.h>
+#include <errno.h>
+
+void parseconfig(const char *file, struct config *cfg)
+{
+    
+}
---
+++ src/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct config
+{
+    
+};
+
+void parseconfig(const char *file, struct config *cfg);
