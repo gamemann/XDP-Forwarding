29b59ca850fdd7fda2d535231e8fda26f19a7bfa
gamemann <gamemann@gflclan.com>
2020-12-30 22:51:24 +0000

    Complete forward add program.

--- src/xdpfwd-add.c
+++ src/xdpfwd-add.c
@@ -4,8 +4,10 @@
 #include <stdlib.h>
 
 #include <bpf.h>
+#include <arpa/inet.h>
 
 #include "xdpfwd.h"
+#include "cmdline.h"
 
 int bpf_map_get(const char *path)
 {
@@ -26,6 +28,64 @@ int main(int argc, char *argv[])
         
         return EXIT_FAILURE;
     }
-    
+
+    // Retrieve command line arguments.
+    struct cmdline cmd = {0};
+
+    parsecmdline(argc, argv, &cmd);
+
+    // Check required arguments.
+    if (cmd.baddr == NULL)
+    {
+        fprintf(stderr, "Missing bind address. Please specify bind address with -b <addr>.\n");
+
+        return EXIT_FAILURE;
+    }
+
+    if (cmd.daddr == NULL)
+    {
+        fprintf(stderr, "Missing destination address. Please specify bind address with -b <addr>.\n");
+
+        return EXIT_FAILURE;
+    }
+
+    uint32_t bindaddr;
+    uint32_t destaddr;
+    uint16_t destport = cmd.dport;
+
+    // Retrieve 32-bit integers of bind and destination addresses in network byte order.
+    struct in_addr baddr;
+    inet_pton(AF_INET, cmd.baddr, &baddr);
+    bindaddr = baddr.s_addr;
+
+    struct in_addr daddr;
+    inet_pton(AF_INET, cmd.daddr, &daddr);
+    destaddr = daddr.s_addr;
+
+    // Check destination port.
+    if (destport < 1)
+    {
+        destport = cmd.bport;
+    }
+
+    // Construct key and values.
+    struct forward_key fwdkey = {0};
+    fwdkey.bindaddr = bindaddr;
+    fwdkey.bindport = htons(cmd.bport);
+    fwdkey.protocol = cmd.protocol;
+
+    struct forward_info fwdinfo = {0};
+    fwdinfo.destaddr = destaddr;
+    fwdinfo.destport = htons(destport);
+
+    if (bpf_map_update_elem(fwdmap, &fwdkey, &fwdinfo, BPF_ANY) != 0)
+    {
+        fprintf(stderr, "Error adding forwarding rule :: %s\n", strerror(errno));
+
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout, "Added forwarding rule %s:%d => %s:%d over protocol %d\n", cmd.baddr, fwdkey.bindport, cmd.daddr, fwdinfo.destport, fwdkey.protocol);
+
     return EXIT_SUCCESS;
 }
