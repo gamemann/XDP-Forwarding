e7367fc8e0e6d62aa975148634ad99388598485a
gamemann <gamemann@gflclan.com>
2021-04-03 23:19:42 +0000

    Add support for saving config file in xdpfwd-del.

--- src/xdpfwd-del.c
+++ src/xdpfwd-del.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <libconfig.h>
 
 #include <bpf.h>
 #include <arpa/inet.h>
@@ -75,6 +76,134 @@ int main(int argc, char *argv[])
     }
 
     fprintf(stdout, "Deleted forwarding rule %s:%d over protocol %s.\n", cmd.baddr, cmd.bport, protocolstr);
+
+    // Check if we're saving.
+    if (cmd.save)
+    {
+        char *cfgfile = "/etc/xdpfwd/xdpfwd.conf";
+
+        if (cmd.cfgfile != NULL)
+        {
+            cfgfile = cmd.cfgfile;
+        }
+
+        // Open file.
+        FILE *fp = fopen(cfgfile, "rw");
+
+        if (fp == NULL)
+        {
+            fprintf(stderr, "Could not save to config file :: %s\n", strerror(errno));
+
+            return EXIT_FAILURE;
+        }
+
+        // Initialize libconfig.
+        config_t conf;
+
+        config_init(&conf);
+
+        // Attempt to read config file through libconfig.
+        if (config_read(&conf, fp) == CONFIG_FALSE)
+        {
+            fprintf(stderr, "Could not save to config file (could not load config) :: %s\n", config_error_text(&conf));
+
+            config_destroy(&conf);
+
+            return EXIT_FAILURE;
+        }
+
+        // We can close the file pointer now.
+        fclose(fp);
+
+        // Attempt to read forwarding rules.
+        config_setting_t *fwd = config_lookup(&conf, "forwarding");
+
+        // If it doesn't exist, create it.
+        if (fwd == NULL)
+        {
+            fprintf(stderr, "Could not save to config file ('forwarding' list does not exist) :: %s\n", config_error_text(&conf));
+
+            config_destroy(&conf);
+
+            return EXIT_FAILURE;
+        }
+
+        // Loop through all rules.
+        for (int i = 0; i < config_setting_length(fwd); i++)
+        {
+            // Retrieve element.
+            config_setting_t *rule = config_setting_get_elem(fwd, i);
+
+            if (rule == NULL)
+            {
+                continue;
+            }
+
+            const char *s_bindaddr;
+
+            if (config_setting_lookup_string(rule, "bind", &s_bindaddr) == CONFIG_FALSE)
+            {
+                continue;
+            }
+
+            int s_bindport = 0;
+            config_setting_lookup_int(rule, "bindport", &s_bindport);
+
+            const char *s_destaddr;
+
+            if (config_setting_lookup_string(rule, "dest", &s_destaddr) == CONFIG_FALSE)
+            {
+                continue;
+            }
+
+            int s_destport = 0;
+            config_setting_lookup_int(rule, "destport", &s_destport);
+
+            const char *s_protocol;
+
+            if (config_setting_lookup_string(rule, "protocol", &s_protocol) == CONFIG_FALSE)
+            {
+                continue;
+            }
+
+            // Now compare.
+            if (s_bindaddr == NULL || strcmp(lowerstr((char *)s_bindaddr), lowerstr(cmd.baddr)) != 0)
+            {
+                continue;
+            }
+
+            if (s_destaddr == NULL || strcmp(lowerstr((char *)s_destaddr), lowerstr(cmd.daddr)) != 0)
+            {
+                continue;
+            }
+
+            if (s_protocol == NULL || strcmp(lowerstr((char *)s_protocol), lowerstr(cmd.protocol)) != 0)
+            {
+                continue;
+            }
+
+            if (protocol != IPPROTO_ICMP && s_bindport != cmd.bport)
+            {
+                continue;
+            }
+
+            if (protocol != IPPROTO_ICMP && s_destport != cmd.dport)
+            {
+                continue;
+            }
+
+            // We found a match, remove this setting.
+            config_setting_remove_elem(fwd, i);
+        }
+
+        // Save config.
+        config_write_file(&conf, cfgfile);
+
+        fprintf(stdout, "Attempted to remove rule from config.\n");
+
+        // Cleanup.
+        config_destroy(&conf);
+    }
 
     return EXIT_SUCCESS;
 }
